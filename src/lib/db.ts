148b0a2be6352ea5bae5d323b32e34801508d90b
bowwow <hibowwowhzk@gmail.com>
2025-11-23 14:13:55 +0900

    Update:add  port

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
-// src/lib/db.ts
 import mysql from 'mysql2/promise';
 
 const db = mysql.createPool({
     host: process.env.DB_HOST!,
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
     user: process.env.DB_USER!,
     password: process.env.DB_PASS!,
     database: process.env.DB_NAME!,
